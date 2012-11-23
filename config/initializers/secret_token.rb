c362343a52f751a519482e7e4805fd68e1e811c4
BrodaSamoZlo <saasbook@saasbook.(none)>
2012-11-23 09:15:48 -0800

    first upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestAppRailsTurtorial::Application.config.secret_token = '77ed6c895cf94e57d06bc7cf125aa3876deb36c026a1766f18197e45446a510ecaf8aa00fe5293952065a170c82ef1ea42defe43ad4fc0127a8115168b1eee46'
